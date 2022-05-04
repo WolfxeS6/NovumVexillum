95140342da0e1eeed03507e7ca7a916ac12a23a7
Lex0r77 <95098511+Lex0r77@users.noreply.github.com>
2022-05-04 11:33:10 +0200

    Defines changes

--- common/defines/NVX_defines_changes.lua
+++ common/defines/NVX_defines_changes.lua
@@ -19,12 +19,16 @@ NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5;
 NDefines.NMilitary.UNIT_EXP_LEVELS = {0.02,	0.04,	0.06,	0.08,	0.1,	0.14,	0.18,	0.22,	0.26,	0.3,	0.39,	0.48,	0.57,	0.66,	0.75,	0.78,	0.81,	0.84,	0.87,	0.9};		-- Experience needed to progress to the next level
 NDefines.NMilitary.EXPERIENCE_COMBAT_FACTOR = 0.05;
 NDefines.NMilitary.ARMY_EXP_BASE_LEVEL = 5;
+NDefines.NMilitary.FIELD_EXPERIENCE_SCALE=     0.004  --0.002 
+NDefines.NMilitary.FIELD_EXPERIENCE_MAX_PER_DAY=     5  --3
+NDefines.NCountry.SPECIAL_FORCES_CAP_BASE= 0.10  --0.05
 NDefines.NBuildings.INFRASTRUCTURE_RESOURCE_BONUS = 0.1;
 -- Fixing minor stuff
 NDefines.NDiplomacy.IDEOLOGY_JOIN_FACTION_MIN_LEVEL = 0.1
 NDefines.NAI.DIPLOMACY_FACTION_SAME_IDEOLOGY_MAJOR = 25
 NDefines.NAI.CALL_ALLY_BASE_DESIRE = 15
 NDefines.NAI.JOIN_ALLY_BASE_DESIRE = 15
+NDefines.NTrade.DISTANCE_TRADE_FACTOR = -0.01 -- -0.02 
 
 -- Designer
 NDefines.NMilitary.MAX_DIVISION_SUPPORT_HEIGHT = 4
